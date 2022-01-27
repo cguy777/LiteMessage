e6143bb3e90894ebb2caa0c894ce947ba13a4c1d
cguy777 <97563947+cguy777@users.noreply.github.com>
2022-01-26 21:48:45 -0500

    Started initial GUI work

---
+++ mtools/apps/litemessage/gui/GUILiteMessage.java
@@ -0,0 +1,7 @@
+package mtools.apps.litemessage.gui;
+
+public class GUILiteMessage {
+	public static void main(String[] args) {
+		MainGUI mGUI = new MainGUI();
+	}
+}
---
+++ mtools/apps/litemessage/gui/MainGUI.java
@@ -0,0 +1,42 @@
+package mtools.apps.litemessage.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+import mtools.apps.litemessage.ContactManager;
+import mtools.apps.litemessage.SettingsModule;
+import mtools.io.MConsole;
+
+public class MainGUI extends JFrame {
+	
+	private ContactManager cMan;
+	
+	public MainGUI() {
+		
+
+		
+		this.setTitle("LiteMessage");
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400, 500);
+		this.setResizable(false);
+		
+		MConsole console = new MConsole();
+		SettingsModule sMod = new SettingsModule(console);
+		
+		cMan = new ContactManager(sMod.getSettings());
+		cMan.loadContacts();
+		
+		
+		
+		String[] s = new String[cMan.getNumContacts()];
+		
+		for(int i = 0; i<cMan.getContacts().size(); i++) {
+			s[i] = cMan.getContacts().get(i).getName();
+		}
+		
+		JList<String> contactList = new JList<String>(s);
+		this.add(contactList);
+		
+	}
+}
