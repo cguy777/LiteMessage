ab287932c50a4b3e5e1ac2a819c9314395472213
cguy777 <97563947+cguy777@users.noreply.github.com>
2022-01-27 20:15:09 -0500

    More GUI integration
    Added some more elements to the main window.  And added functionality to the connect button so that it launches a new window with the corresponding chat session.

--- mtools/apps/litemessage/gui/MainGUI.java
+++ mtools/apps/litemessage/gui/MainGUI.java
@@ -1,8 +1,16 @@
 package mtools.apps.litemessage.gui;
 
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 
+import mtools.apps.litemessage.Contact;
 import mtools.apps.litemessage.ContactManager;
 import mtools.apps.litemessage.SettingsModule;
 import mtools.io.MConsole;
@@ -10,23 +18,26 @@ import mtools.io.MConsole;
 public class MainGUI extends JFrame {
 	
 	private ContactManager cMan;
+	private JList<String> contactList;
+	private JButton messageButton;
 	
 	public MainGUI() {
 
-		this.setVisible(true);
+		//Main GUI Frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(400, 500);
 		this.setResizable(false);
+		this.setLayout(null);
 		
 		MConsole console = new MConsole();
 		SettingsModule sMod = new SettingsModule(console, false);
 		
 		this.setTitle("LiteMessage - " + sMod.getSettings().thisUser.getName());
 		
+		
+		//Contacts List
 		cMan = new ContactManager(sMod.getSettings());
 		cMan.loadContacts();
-		
-		
 		
 		String[] s = new String[cMan.getNumContacts()];
 		
@@ -34,8 +45,50 @@ public class MainGUI extends JFrame {
 			s[i] = cMan.getContacts().get(i).getName();
 		}
 		
-		JList<String> contactList = new JList<String>(s);
+		contactList = new JList<String>(s);
+		
+		contactList.setSize(100, 500);
+		contactList.setBounds(10, 10, 120, 300);
 		this.add(contactList);
+		
+		
+		//Connect To Button
+		messageButton = new JButton("Connect To...");
+		messageButton.setVisible(true);
+		messageButton.setBounds(10, 320, 120, 20);
+		messageButton.addActionListener(new ConnectButtonAction());
+		this.add(messageButton);
+		
+		
+		
+		
+		
+		this.setVisible(true);
+		
+	}
+	
+	private class ConnectButtonAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			Contact selectedContact;
+			MessagingGUI mGUI;
+			
+			try {
+				
+				selectedContact = cMan.getContactByName(contactList.getSelectedValue());
+				mGUI = new MessagingGUI(selectedContact, cMan);
+			
+			} catch(Exception ex) {
+				//Nothing is selected.  So do nothing.
+				return;
+			}
+			
+			
+		}
 		
 	}
 }
+
+
