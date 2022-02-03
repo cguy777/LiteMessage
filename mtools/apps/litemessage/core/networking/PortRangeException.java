7db04a915c53394ae4198cbe88ad6ba912811f8e
cguy777 <97563947+cguy777@users.noreply.github.com>
2022-02-03 08:46:07 +0300

    Started work on a new exception type

---
+++ mtools/apps/litemessage/core/networking/PortRangeException.java
@@ -0,0 +1,11 @@
+package mtools.apps.litemessage.core.networking;
+
+public class PortRangeException extends Exception {
+	public PortRangeException() {
+		
+	}
+	
+	public PortRangeException(String s) {
+		super(s);
+	}
+}
