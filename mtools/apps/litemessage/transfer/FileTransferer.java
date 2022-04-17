53fe471d185063ddb15b33211c93257a79d1b84e
cguy777 <97563947+cguy777@users.noreply.github.com>
2022-04-16 23:42:21 -0400

    Update FileTransferer.java

--- mtools/apps/litemessage/transfer/FileTransferer.java
+++ mtools/apps/litemessage/transfer/FileTransferer.java
@@ -1,5 +1,10 @@
 package mtools.apps.litemessage.transfer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import mtools.apps.litemessage.core.networking.StreamBundle;
 
 public class FileTransferer {
@@ -8,6 +13,30 @@ public class FileTransferer {
 	
 	public FileTransferer(StreamBundle bundle) {
 		sBundle = bundle;
+	}
+	
+	/**
+	 * Sends a file to the other client
+	 * @param file
+	 */
+	public void sendFile(File file) {
+		try {
+			FileReader fReader = new FileReader(file);
+			BufferedReader bReader = new BufferedReader(fReader);
+			
+			while(true) {
+				int data = fReader.read();
+				
+				if(data == -1) {
+					break;
+				}
+				
+				sBundle.getOutputStream().writeByte(data);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void receiveFile() {
