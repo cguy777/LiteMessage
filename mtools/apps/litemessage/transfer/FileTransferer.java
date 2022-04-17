2bb255f5e37edc95eacaa10f1901960f139505f2
cguy777 <97563947+cguy777@users.noreply.github.com>
2022-04-16 23:22:59 -0400

    Added Fileransferer

---
+++ mtools/apps/litemessage/transfer/FileTransferer.java
@@ -0,0 +1,16 @@
+package mtools.apps.litemessage.transfer;
+
+import mtools.apps.litemessage.core.networking.StreamBundle;
+
+public class FileTransferer {
+	
+	StreamBundle sBundle;
+	
+	public FileTransferer(StreamBundle bundle) {
+		sBundle = bundle;
+	}
+	
+	public void receiveFile() {
+		
+	}
+}
